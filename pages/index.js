f0a0014c76d9740b511688a6e58775de581dba1d
CZanardi <54423621+CZanardi@users.noreply.github.com>
2024-12-01 13:28:44 +0000

    criação da página `Em construção`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,30 @@
 function Home() {
   return (
-    <h1>Fala, Israeeeel!!!!! Agora seu nome está num site... bora codar???</h1>
+    (
+      <h1>
+        <em>Em construção</em>
+      </h1>
+    ),
+    (
+      <h2>
+        Esta página será um repositório para centralizar meu portfólio e um
+        ambiente em que seja possível testar novas ideias.
+      </h2>
+    ),
+    (<h2>Deve ser acessada por pessoas interessadas em me contratar.</h2>),
+    (
+      <h3>
+        Aqui você encontrará uma explicação com a ótica de negócios e o link
+        para o projeto real.
+      </h3>
+    ),
+    (<h3>Dúvidas e sugestões serão sempre bem-vindas e incentivadas!</h3>),
+    (
+      <h3>
+        Será necessário sempre ir construindo novos projetos e atualizar a
+        página.
+      </h3>
+    )
   );
 }
 
